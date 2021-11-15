55e930337b8842a6abbe6f37c3bc342eb100d927
Serban <niculina.serban.ext@siemens.com>
2021-11-15 16:29:30 +0200

    Added observation time for forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,'' + body.current.weather_descriptions[0] + ', temperature: ' + body.current.temperature + ', feelsLike: ' + body.current.feelslike + '')
+            callback(undefined,'' + body.current.weather_descriptions[0] + ', temperature: ' + body.current.temperature + ', feelsLike: ' + body.current.feelslike + ', time:' + body.current.observation_time)
         }
     })
 }
